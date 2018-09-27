0da1739e992ae015d37774ac65de3637a8e3bd0b
Mahesh Mindrops <maheshmindrops@gmail.com>
2018-09-27 11:36:10 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+const classroomController = require('../controllers').classroom;
+router.get('/api/classroom', classroomController.list);
+router.get('/api/classroom/:id', classroomController.getById);
+router.post('/api/classroom', classroomController.add);
+router.put('/api/classroom/:id', classroomController.update);
+router.delete('/api/classroom/:id', classroomController.delete);
+
+const studentController = require('../controllers').student;
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
+
+const lecturerController = require('../controllers').lecturer;
+router.get('/api/lecturer', lecturerController.list);
+router.get('/api/lecturer/:id', lecturerController.getById);
+router.post('/api/lecturer', lecturerController.add);
+router.put('/api/lecturer/:id', lecturerController.update);
+router.delete('/api/lecturer/:id', lecturerController.delete);
+
+const courseController = require('../controllers').course;
+router.get('/api/course', courseController.list);
+router.get('/api/course/:id', courseController.getById);
+router.post('/api/course', courseController.add);
+router.put('/api/course/:id', courseController.update);
+router.delete('/api/course/:id', courseController.delete);
+
+router.post('/api/classroom/add_with_students', classroomController.addWithStudents);
